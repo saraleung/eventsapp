d375e053c2eca67b09e0e5b6c3fbab830bc48a8c
Sara Leung <sara.leung@zafin.com>
2017-05-27 11:48:51 -0400

    added logging

--- home.py
+++ home.py
@@ -1,10 +1,13 @@
 import os
+import logging
 from flask import Flask
 
 app = Flask(__name__)
+logging.basicCongig(level=logging.DEBUG)
 
 @app.route('/')
 def hello():
+		logging.debug("saying hello")
 		return 'Hello EventApp!'
 
 if __name__ == "__main__":
