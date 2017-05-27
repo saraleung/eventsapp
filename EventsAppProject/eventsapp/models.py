be6b5e03ac338ce940f9a6c6120a619c47ead01b
Sara Leung <saraleung@users.noreply.github.com>
2017-05-27 18:06:21 -0400

    added table in index.html

--- EventsAppProject/eventsapp/models.py
+++ EventsAppProject/eventsapp/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 # Create your models here.
-
-class Performer(models.Model):
-	name = models.CharField(max_length  = 50)
-
 class Event(models.Model):
 	name = models.CharField(max_length  = 250)
 	datetime = models.DateTimeField()
-	performer= models.ForeignKey(Performer, on_delete=models.CASCADE)
+
+class Performer(models.Model):
+	name = models.CharField(max_length  = 50)
+	event = models.ForeignKey(Event, default='', on_delete=models.CASCADE)
+
+
 
--- EventsAppProject/eventsapp/views.py
+++ EventsAppProject/eventsapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from eventsapp.models import Event
+from eventsapp.models import Event, Performer
 # Create your views here.
 def home(request):
-	return render(request, 'index.html')
+	events = Event.objects.all()
+	performers = Performer.objects.all()
+	return render(request, 'index.html', {'events' : events, 'performers' : performers})
