5c25e68a9da14a69e2668e64b8eb0a13c40f71c5
Sara Leung <sara.leung@zafin.com>
2017-05-29 01:59:41 -0400

    inital commit to reset project

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn eventsapp.wsgi --log-file -
--- main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from eventsapp.models import Event, Performer
-# Create your views here.
-def home(request):
-	events = Event.objects.all()
-	performers = Performer.objects.all()
-	return render(request, 'index.html', {'events' : events, 'performers' : performers})
